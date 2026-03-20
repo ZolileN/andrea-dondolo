8c6c1999bf8bbce8161d37894a9cb10b87d8ba9f
spanateam <zolile@mlkcomputer.co.za>
2026-03-20 18:43:28 +0200

    replacing the unsupported next-sanity/live usage with a compatibility shim

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,9 +1,14 @@
-// Querying with "sanityFetch" will keep content automatically updated
-// Before using it, import and render "<SanityLive />" in your layout, see
-// https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity/live";
 import { client } from './client'
 
-export const { sanityFetch, SanityLive } = defineLive({
-  client,
-});
+type SanityFetchOptions = {
+  query: string
+  params?: Record<string, unknown>
+}
+
+export async function sanityFetch<T>({ query, params = {} }: SanityFetchOptions): Promise<T> {
+  return client.fetch<T>(query, params)
+}
+
+export function SanityLive(): null {
+  return null
+}

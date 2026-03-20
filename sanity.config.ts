ad221452f187fa9a526f8866009920dd954ee601
spanateam <zolile@mlkcomputer.co.za>
2026-03-20 17:38:11 +0200

    Studio functioning

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,11 +6,13 @@ import { structure } from './sanity/structure/deskStructure'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION!
 
 export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
+  apiVersion,
   title: 'Andrea Dondolo',
   schema: {
     types: schemaTypes,
